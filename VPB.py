aeb54b0503c07169a5a7ec3282880ff4fdbf6205
Rémi Tournebize <remi.tournebize@gmail.com>
2016-01-17 11:32:17 +0100

    VPB 2.0
    embedded the initialization traversing inside the speciation spreading
    traversing: around 30% of speed gain

--- VPB.py
+++ VPB.py
@@ -2,7 +2,7 @@
 
 # -*- coding: utf-8 -*-
 """
-VANAPRABHAVA v.2
+VANAPRABHAVA v.2.1
 
 Created on Wed Dec 23 17:33:31 2015
 @author: Remi Tournebize
@@ -13,8 +13,9 @@ v1.2: more profiling than v1.1 resulting in a significant gain of speed
 v1.3_ms: few debuggings on the SFS (duplicates)
 v1.4: debugged issues related to time decimal precision in Newick strings + modified force_ultrametricity module
 v1.5 31122015: changed binomial to poisson random (large branch lengths throw an error on C long type for binomial)
-v1.6 07012015: prints out the number of species 1 line AFTER the newick-formatted phylo tree in the .newick output files
-v2.0 08012015: MAJOR CHANGE in the traversing function to convert demo into phylo: huge gain of speed for megademographies
+v1.6 07012016: prints out the number of species 1 line AFTER the newick-formatted phylo tree in the .newick output files
+v2.0 08012016: MAJOR CHANGE in the traversing function to convert demo into phylo: huge gain of speed for megademographies / runtime will be independent from the value of the mutation rate
+v2.1 09012016: embedded the initialization traversing inside the speciation spreading traversing: around 30% of speed gain
 """
 
 from ete3 import Tree
@@ -54,7 +55,9 @@ parser.add_argument('-osfs', '--SFSoutput', type=str, required=True,
 parser.add_argument('-I', '--islands', type=int, nargs="+", default=-9,
                     action="store", help="Number of individuals in each deme separated by a comma, must be in the same order as in your MS command")
 parser.add_argument('--forceUltrametric',
-                    action="store_true", help="add this option to force the input tree to be ultrametric (useful if you rescale branch lengths)")
+                    action="store_true", help="add this option to force the input tree to be ultrametric (useful if you rescale branch lengths), but can slow the execution if too many final species are present")
+parser.add_argument('--maxNumberOfSpecies', type=int, required=False,
+                    action="store", default=-1, help="if using --forceUltrametric, ignore the ultrametric forcing if the number of species is above this value (speeds up the execution)s")
 parser.add_argument('--drawTrees',
                     action="store_true", help="add this option to draw the analyzed trees in external files (same dir as PHYLOoutput)")
 parser.add_argument('--seed', type=int, default=-1,
@@ -74,6 +77,7 @@ scale = args.scalingFactor #ok
 ophylo = args.PHYLOoutput #ok
 osfs = args.SFSoutput #ok
 force_ultrametric = args.forceUltrametric #ok
+maxNumberOfSpecies = args.maxNumberOfSpecies
 plot_trees = args.drawTrees #ok
 seed = args.seed #ok
 quiet = args.quiet #ok
@@ -104,15 +108,16 @@ if not quiet:
 """
 
 
-t="C:/Users/Windows/Desktop/TRAVAIL FM/VANAPRABHAVA/VPB_DELL/temp/treeORI.txt"
-mu= 0.000000067256260017746 #0.00000067256260017746
+t="C:/Users/Windows/Desktop/TRAVAIL FM/VANAPRABHAVA/VPB_DELL/temp/test.txt"
+mu= 0.0000067256260017746 #0.00000067256260017746
 scale=2e+10
 ophylo="C:/Users/Windows/Desktop/TRAVAIL FM/VANAPRABHAVA/VPB_DELL/phylo.txt"
 osfs="C:/Users/Windows/Desktop/TRAVAIL FM/VANAPRABHAVA/VPB_DELL/sfs.txt"
 force_ultrametric=True
-plot_trees=False
+maxNumberOfSpecies = 2000
+plot_trees=True
 quiet=False
-ms_islands=[20000,20000,20000] #[20000,20000,20000]
+ms_islands=[200,100,200] #[20000,20000,20000]
 ms_input=True
 numpy.random.seed(10)
 
@@ -130,34 +135,34 @@ def ubranch_mutation(node, mu):
 
 """ #v1.6
 def leaf_species_dictionary(t):
-    SPdict = {}
-    SPcounts = {}
-    for leaf in t:
-        sp = leaf.sp
-        if sp in SPdict:
-            SPdict[sp].append(leaf)
-            SPcounts[sp] += 1
-        else:
-            SPdict[sp] = [leaf]
-            SPcounts[sp] = 1
-    return SPdict, SPcounts
+	SPdict = {}
+	SPcounts = {}
+	for leaf in t:
+		sp = leaf.sp
+		if sp in SPdict:
+			SPdict[sp].append(leaf)
+			SPcounts[sp] += 1
+		else:
+			SPdict[sp] = [leaf]
+			SPcounts[sp] = 1
+	return SPdict, SPcounts
 
 def range_by_length(start, end, length):
-    step = ( end - start ) / (1.*length)
-    L = list(numpy.arange(start, end, step))
-    for l in range(0, len(L)):
-        L[l] = int(round(L[l]))
-    return L
+	step = ( end - start ) / (1.*length)
+	L = list(numpy.arange(start, end, step))
+	for l in range(0, len(L)):
+		L[l] = int(round(L[l]))
+return L
 """
 
 def get_deme(tip_id, ms_islands):
-    tip_id = int(tip_id)
-    pop = numpy.where(tip_id <= ms_islands)
-    if len(pop[0]) == 0:
-        sys.exit("1. The MS island structure you provided through -I does not fit the number of tips in the demography. The option might have been mispecified. Check it out!")
-        return(None)
-    else:
-        return(pop[0][0] + 1)
+	tip_id = int(tip_id)
+	pop = numpy.where(tip_id <= ms_islands)
+	if len(pop[0]) == 0:
+		sys.exit("1. The MS island structure you provided through -I does not fit the number of tips in the demography. The option might have been mispecified. Check it out!")
+		return(None)
+	else:
+		return(pop[0][0] + 1)
 
 
 
@@ -173,66 +178,78 @@ sys.stdout.write('[')
 t = Tree(t, format=5)
 sys.stdout.write('R') # Read
 
+#======================================================#
+# MS island initialization
+if ms_input:
+	ms_islands = numpy.array(ms_islands)
+	ms_islands = numpy.cumsum(ms_islands)
+	nPops = len(ms_islands)
 
 #======================================================#
-# SET at once: "names" of inner nodes && initialize "species" attributes
-if ms_input:
-    ms_islands = numpy.array(ms_islands)
-    ms_islands = numpy.cumsum(ms_islands)
-    nPops = len(ms_islands)
-
-innode = 0
-largest_id = 0
-nIndsORI = 0
-for node in t.traverse():
-    node.add_features(sp=1)
-    node.img_style["size"] = 0
-    if not node.is_leaf():
-        node.name = "iN%d" %innode
-        innode += 1
-    else:
-        nIndsORI += 1
-        if ms_input == True:
-           inn = int(node.name)
-           if inn > largest_id: largest_id = inn
-           pop = get_deme(inn, ms_islands)
-           node.name = str(pop)+"_"+node.name
-           node.add_features(mergedInd=node.name)
-    if not node.is_root():
-        node.dist *= scale
-
-if (ms_input == True) and (ms_islands[-1] > largest_id):
-    sys.exit("2. The MS island structure you provided through -I does not fit the number of tips in the demography. The option might have been mispecified. Check it out!")
-
-# set here the input tree to ultrametric?
+# set (here) the input tree to ultrametric?
 # absolute ultrametricity does not matter for topological manipulations.
 # It could possibly affect the speciational spreading but, in general, timescales are so big
 # that a lack in ultrametricity due to F * precision would eventually have insignificant impacts
 
-if plot_trees: t.render(ophylo+"_1ORIGINAL.png", w=183, units="mm")
-sys.stdout.write('T') # Transform
-
-
 #======================================================#
-# SPREAD speciation events
+# INITIALIZE tree & SPREAD speciation events
+
+innerNodeIndex = 0
+largest_id = 0
+nIndsORI = 0
 spID = 0
 for node in t.traverse("preorder"):
-    # if leaf: NO speciation
-    if not node.is_leaf():
-        umut = ubranch_mutation(node, mu)
-        if umut == True:
-            spID += 1
-            node.sp = spID
-            for leaf in node:
-                leaf.sp = spID
-sys.stdout.write('SS') # SpreadSpeciation
+
+	# INITIALIZE
+
+	## add "sp" feature
+	try:
+		node.sp
+	except AttributeError:
+		node.add_features(sp=1)
+	#node.img_style["size"] = 0
+
+	## name the inner nodes
+	if not node.is_leaf():
+		node.name = "n%d" %innerNodeIndex
+		innerNodeIndex += 1
+	else:
+		nIndsORI += 1
+		if ms_input == True:
+			nID = int(node.name)
+			if nID > largest_id: largest_id = nID
+			pop = get_deme(nID, ms_islands)
+			node.name = str(pop)+"_"+node.name
+			node.add_features(mergedInd=node.name)
+
+	# SCALE
+	if not node.is_root():
+		node.dist *= scale
+
+	# SPREAD SPECIATIONS
+	## if leaf: NO speciation
+	if not node.is_leaf():
+		umut = ubranch_mutation(node, mu)
+		if umut == True:
+			spID += 1
+			node.sp = spID
+			for leaf in node:
+				try:
+					leaf.sp = spID
+				except AttributeError:
+					leaf.add_features(sp=1)
+
+if (ms_input == True) and (ms_islands[-1] > largest_id):
+	sys.exit("2. The MS island structure you provided through -I does not fit the number of tips in the demography. The option might have been mispecified. Check it out!")
+
+sys.stdout.write('S') # SpreadSpeciation
 
 # if requested, print speciational tree
 if plot_trees:
-    tmut = t.copy()
-    for leaf in tmut:
-        leaf.name = "["+str(leaf.sp)+"]"+leaf.name
-    tmut.render(ophylo+"_2MUT.png", w=183, units="mm")
+	tmut = t.copy()
+	for leaf in tmut:
+		leaf.name = "["+str(leaf.sp)+"]"+leaf.name
+	tmut.render(ophylo+"_2MUT.png", w=183, units="mm")
 
 
 #======================================================#
@@ -253,11 +270,11 @@ for node in t.traverse("preorder"):
 
 			# get child.1's tip species labels
 			csp1 = set()
-			for i in children[0]:
+			for i in children[0].iter_leaves():
 				csp1.add(i.sp)
 			# get child.2's tip species labels
 			csp2 = set()
-			for i in children[1]:
+			for i in children[1].iter_leaves():
 				csp2.add(i.sp)
 
 			# compare species label between children
@@ -268,22 +285,28 @@ for node in t.traverse("preorder"):
 
 				if not node.is_root():
 
-					# populate "mergedInd" feature for future SFS
-					mergedLeaves = ""
-					for l in node.iter_leaves():
-						mergedLeaves = mergedLeaves+" "+l.name
-
 					# collapse the subtree
 					upNode = node.up
+
+					# find the new leaf
+					# NB. using get_distance() is slower
 					newLeaf = node.get_farthest_leaf()
 					newDist = newLeaf[1] + node.dist
 
+					mergedLeaves = ""
+					firstLeafDone = False
 					for childnode in node.traverse():
+						# add all the nodes further ignored by the algo
 						traversedNodes.add(childnode)
-					bad = node.detach()
+						# populate "mergedInd" feature for the new leaf
+						if childnode.is_leaf():
+							mergedLeaves = mergedLeaves+" "+childnode.name
+
+					# detach the subtree and add the new leaf
+					node.detach()
 					upNode.add_child(newLeaf[0], newLeaf[0].name, newDist)
 
-					# the node.prune() function is too lenghty
+					# the node.prune() function is too slow
 
 					# actualize "mergedInd" feature of new leaf
 					newLeaf[0].mergedInd = mergedLeaves
@@ -304,21 +327,22 @@ for node in t.traverse("preorder"):
 					# actualize "mergedInd" feature of new leaf
 					newLeaf[0].mergedInd = mergedLeaves
 
-sys.stdout.write('CCCC')
 nTrueSpecies = len(t)
+sys.stdout.write('C')
 
 #======================================================#
-# COMPUTE & PRINT the SFS (full names & numerical)
+# COMPUTE & EXPORT the SFS (full names & numerical)
 
 f = open(osfs+"_allinds.txt", 'w+')
 fn = open(osfs, 'w+')
 fn.write("Tip_label\t" + "\t".join("Isl_"+str(x) for x in range(0, nPops)) + "\n")
 
+nIndsSFS = 0
 for leaf in t.iter_leaves():
 	inds = leaf.mergedInd.lstrip(" ")
 	# write the SFS with full names
 	f.write(leaf.name+" : "+inds+"\n")
-	# compute the per-deme numerical SFS
+	# compute the per-deme numerical SFS and write
 	sfs = [0] * nPops
 	inds = inds.split(" ")
 	for i in inds:
@@ -329,23 +353,26 @@ for leaf in t.iter_leaves():
 
 f.close()
 fn.close()
-
+sys.stdout.write('S')
 
 #======================================================#
-
-# ultrametricity in the output tree is defined in reference to the furthest leaf
+# FORCE ULTRAMETRIC in the output tree, defined in reference to the furthest leaf
+if maxNumberOfSpecies == -1: maxNumberOfSpecies = nTrueSpecies + 1
 if force_ultrametric:
-	tree_dist = t.get_farthest_leaf()[1]
-	for l in t:
-		dst = t.get_distance(l)
-		if dst != tree_dist:
-			l.dist += tree_dist - dst
+	if nTrueSpecies <= maxNumberOfSpecies:
+		tree_dist = t.get_farthest_leaf()[1]
+		for l in t:
+			dst = t.get_distance(l)
+			if dst != tree_dist:
+				l.dist += tree_dist - dst
+		sys.stdout.write('u')
+	else:
+		sys.stdout.write('\nWill not go ultrametric: too many final species!\n')
 
 #======================================================#
-
-## print PHYLO
+# EXPORT phylo
 t.write(format=5, outfile=ophylo, dist_formatter='%0.20f')
-# write the count of resultant species
+## write the count of resultant species
 f = open(ophylo, 'a')
 f.write("\n"+str(nTrueSpecies)+"\n")
 f.close()
@@ -354,13 +381,14 @@ if plot_trees:
 	for leaf in t:
 		leaf.name = leaf.sp
 	t.render(ophylo+"_3PHYLO.png", w=183, units="mm")
-"""
-sys.stdout.write('P] ') # Print
-if not quiet: print "            >> " + str(time.time() - init) + " sec"
-if not quiet: print "Total species count      >> " + str(len(SFS))
-if (len(SFS) == nTrueSpecies) and (nIndsSFS == nIndsORI):
+
+#======================================================#
+# FINAL PROMPTS
+sys.stdout.write('P]\n') # Print
+if not quiet: print "Runtime                  >> " + str(time.time() - init) + " sec"
+if not quiet: print "Total species count      >> " + str(nTrueSpecies)
+if nIndsSFS == nIndsORI:
 	print "Exit status              >> OK!"
 else: sys.exit("ERROR. A bug must be crawling around... Please, please, contact the programmer.")
 
-"""
 
